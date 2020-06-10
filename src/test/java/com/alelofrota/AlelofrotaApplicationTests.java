eb254605a4f0a0b845f269aa79854e36d12c3b3f
camila-soares <camilasoares1507@gmail.com>
2020-06-10 02:04:07 -0300

    ajustes na classe principal de test

--- src/test/java/com/alelofrota/AlelofrotaApplicationTests.java
+++ src/test/java/com/alelofrota/AlelofrotaApplicationTests.java
@@ -1,13 +1,17 @@
 package com.alelofrota;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class AlelofrotaApplicationTests {
+public class AlelofrotaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
